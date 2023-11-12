7e1acd1a4055dc6ac5cc89e27cfb41770e6cdc0d
jay-mothata <joey.mothata@gmail.com>
2023-11-12 07:35:31 -0800

    A  function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,12 +1,34 @@
 #include "main.h"
 
 /**
+ * write_char - Helper function to write a single character to stdout
+ * @c: Character to be written
+ * @output_count: Pointer to the count of characters written
+ */
+static void write_char(char c, int *output_count)
+{
+	write(1, &c, 1);
+	(*output_count)++;
+}
+
+/**
+ * print_char - Handle the 'c' specifier in the format string
+ * @ls_args: Argument list
+ * @output_count: Pointer to the count of characters written
+ */
+static void print_char(va_list ls_args, int *output_count)
+{
+	int character = va_arg(ls_args, int);
+
+	write_char(character, output_count);
+}
+
+/**
  * _printf - a function that produces output according to a format
  * @format: character string (format string)
  *
  * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list ls_args;
@@ -19,37 +41,20 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			output_of_chars++;
+			write_char(*format, &output_of_chars);
 		}
 		else
 		{
 			switch (*(++format))
 			{
-				case 'c':
-					{
-						int character = va_arg(ls_args, int);
-
-						write(1, &character, 1);
-						output_of_chars++;
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(ls_args, char *);
-						int length = strlen(str);
-
-						write(1, str, length);
-						output_of_chars += length;
-						break;
-					}
-				case '%':
-					write(1, "%", 1);
-					output_of_chars++;
-					break;
-
-				default:
-					break;
+			case 'c':
+				print_char(ls_args, &output_of_chars);
+				break;
+			case '%':
+				write_char('%', &output_of_chars);
+				break;
+			default:
+				break;
 			}
 		}
 		format++;
