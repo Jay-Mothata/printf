e65b52da986ca1e6a6a0a637f890f0bf85826db3
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-13 00:52:34 -0800

    Refactored the code

--- _printf.c
+++ _printf.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- * write_char - Helper function to write a single character to stdout
- * @c: Character to be written
- * @output_count: Pointer to the count of characters written
+ * print_char - Helper function to write a single character to stdout
+ * @character: Character to be written
  */
-static void write_char(char c, int *output_count)
+
+void print_char(char character)
 {
-	write(1, &c, 1);
-	(*output_count)++;
+	write(1, &character, 1);
 }
 
 /**
- * print_char - Handle the 'c' specifier in the format string
- * @ls_args: Argument list
- * @output_count: Pointer to the count of characters written
+ * print_string - Helps to print a string of a specified length
+ * @str: String to be printed
+ * @length: Length of string
  */
-static void print_char(va_list ls_args, int *output_count)
-{
-	int character = va_arg(ls_args, int);
 
-	write_char(character, output_count);
+void print_string(const char *str, int length)
+{
+	write(1, str, length);
 }
 
 /**
@@ -29,37 +27,44 @@ static void print_char(va_list ls_args, int *output_count)
  *
  * Return: the number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	va_list ls_args;
-	int output_of_chars = 0;
 
 	va_start(ls_args, format);
+
+	int printed_chars = 0; /*Counter for printed chars*/
 
 	while (*format)
 	{
 		if (*format != '%')
 		{
-			write_char(*format, &output_of_chars);
+			print_char(*format);
+			printed_chars++;
 		}
 		else
 		{
-			switch (*(++format))
+			format++;
+			if (*format == 'c' || *format == '%')
 			{
-			case 'c':
-				print_char(ls_args, &output_of_chars);
-				break;
-			case '%':
-				write_char('%', &output_of_chars);
-				break;
-			default:
-				break;
+				print_char((*format == 'c') ? va_arg(ls_args, int) : '%');
+				printed_chars++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(ls_args, const char *);
+
+				while (*str)
+				{
+					print_char(*str);
+					str++;
+					printed_chars++;
+				}
 			}
 		}
 		format++;
 	}
-
-	va_end(ls_args);
-
-	return (output_of_chars);
+	va_end(ls_args); /*CLeans up variable arguements*/
+	return (printed_chars);/*Returns the total chars printed*/
 }
