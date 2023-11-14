28643510b9b9f95ce6d0a108773755ba78ce192a
jay-mothata <joey.mothata@gmail.com>
2023-11-14 12:52:51 -0800

    Main Header

--- main.h
+++ main.h
@@ -11,6 +11,7 @@
 #define CHAR_TYPE 1
 #define STRING_TYPE 2
 #define PERCENT_TYPE 3
+#define BINARY_TYPE 5
 
 /* Custom _putchar function for printing to stdout */
 int _putchar (char c);
@@ -23,5 +24,8 @@ void print_char(char charater);
 /* Prototypes for handling %d and %i specifiers */
 int _print_number(const char *format, ...);
 int print_int(int num);
+
+/*Protyotype fo handling %b specifiers */
+int print_binary(unsigned int n);
 
 #endif /*MAIN_H */
