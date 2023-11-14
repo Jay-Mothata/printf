1db8467e8b0b964028d1f1772c83a20c548c5885
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-13 21:40:54 -0800

    Added integer constants

--- main.h
+++ main.h
@@ -6,10 +6,14 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define CHAR_TYPE 1
+#define STRING_TYPE 2
+#define PERCENT_TYPE 3
 
 int _printf(const char *format, ...);
+int specifier_handler(int specidfier, va_list args);
 void print_char(char charater);
-int print_string(const char *str);
-int print_percentage(void);
 
 #endif /* MAIN_H */
