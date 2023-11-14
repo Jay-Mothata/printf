ea299ba3b77b0095ad04c3b74af309accc9b81bb
jay-mothata <joey.mothata@gmail.com>
2023-11-14 07:56:37 -0800

    Main Header

--- main.h
+++ main.h
@@ -11,13 +11,12 @@
 #define CHAR_TYPE 1
 #define STRING_TYPE 2
 #define PERCENT_TYPE 3
+#define INT_TYPE 4
 
 int _printf(const char *format, ...);
 int specifier_handler(int specidfier, va_list args);
 void print_char(char charater);
-
-/* New function prototype for print_int */
-void print_integer(va_list args);
 int _putchar (char c);
+int print_int(int num);
 
 #endif /*MAIN_H */
