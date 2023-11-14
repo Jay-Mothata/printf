909dc25cfd2618dfc61fca390c1ad7b458b2eef4
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-14 10:30:41 -0800

    Added prototypes to the header file

--- main.h
+++ main.h
@@ -11,12 +11,17 @@
 #define CHAR_TYPE 1
 #define STRING_TYPE 2
 #define PERCENT_TYPE 3
-#define INT_TYPE 4
 
+/* Custom _putchar function for printing to stdout */
+int _putchar (char c);
+
+/* Prototypes for handling %c, %s, %d specifiers */
 int _printf(const char *format, ...);
 int specifier_handler(int specidfier, va_list args);
 void print_char(char charater);
-int _putchar (char c);
+
+/* Prototypes for handling %d and %i specifiers */
+int _print_number(const char *format, ...);
 int print_int(int num);
 
 #endif /*MAIN_H */
